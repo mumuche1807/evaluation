c409b6055ab373e3c54869a6eaf13ca75b5053e2
Antoine Buchet <antoinebuchet62136@gmail.com>
2022-03-25 11:02:16 +0100

    correction du fichier cinema.sql

--- base de donnee/cinema.sql
+++ base de donnee/cinema.sql
@@ -1,7 +1,7 @@
 create database cinema CHARACTER SET utf8 ;
 use cinema ;
 
-/*création de la table cinema */
+
 
 create table cinema (
     id int not null primary key auto_increment,
@@ -9,7 +9,7 @@ create table cinema (
     ville varchar(50) not null
 ) ;
 
-/*creation de la table salle*/
+
 
 create table salle (
     id int not null primary key auto_increment,
@@ -17,7 +17,7 @@ create table salle (
     nb_siege int not null
 ) ;
 
-/*creation de la table tarif*/
+
 
 create table tarif (
     id int not null primary key auto_increment,
@@ -25,7 +25,7 @@ create table tarif (
     prix decimal(4,2) not null
 ) ;
 
-/*creation de la table seance*/
+
 
 create table seance (
     id int not null primary key auto_increment,
@@ -39,7 +39,7 @@ create table seance (
     foreign key (id_cinema) references cinema(id)
 ) ;
 
-/*gestion des utilisateurs*/
+
 
 create user 'administrateur'@'localhost' IDENTIFIED BY 'admin' ;
 GRANT SELECT, INSERT, UPDATE, DELETE ON cinema.* TO 'administrateur'@'localhost' ;
@@ -47,22 +47,19 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON cinema.* TO 'administrateur'@'localhost'
 create user 'utilisateur'@'localhost' IDENTIFIED BY '1234' ;
 GRANT INSERT ON cinema.seance TO 'utilisateur'@'localhost' ;
 
-/*alimentation des tables*/
 
-/*permet d'alimenter la table tarif avec 3 tarifs différents*/
 
 insert into tarif (nom, prix) VALUES ('plein tarif', '9.20'),
                                      ('étudiant', '7.60'),
                                      ('moins de 14 ans', '5.90');
 
-/*permet d'ajouter 5 salle */
+
 insert into salle (num_salle, nb_siege) VALUES  (001, 100),
                                                 (002, 150),
                                                 (003, 125),
                                                 (004, 225),
                                                 (005, 200);
 
-/*permet d'ajouter trois cinéma*/
 
 insert into cinema (nom, ville)
 VALUES ('Kinepolis','Lomme'),
@@ -70,14 +67,13 @@ VALUES ('Kinepolis','Lomme'),
        ('Etoile Cinémas', 'Béthune');
 
 
-/*permet d'ajouter les séances*/
-
 insert into seance (film, id_tarif, id_salle, id_cinema, date) VALUES ('fight club', 1, 2, 3, '1999-11-10 21:00:00') ,
                                                                       ('interstellar', 3, 5, 3, '2005-11-05 18:00:00'),
                                                                       ('inception', 2, 1, 2, '2010-07-21 11:30:00'),
                                                                       ('pulp fiction', 1, 3, 1, '1994-10-26 16:00:00' ),
                                                                       ('pulp fiction', 1, 4, 1, '1994-10-26 16:00:00' );
-/*permet d'afficher les détails de chaque seance*/
+
+
 select seance.film as 'film',
        seance.date as 'jour et heure de la séance',
        concat_ws(' à ',c.nom,c.ville) as 'nom et ville du cinéma' ,
